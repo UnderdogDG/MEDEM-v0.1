3cf38bf94167f7d13c74a565090a8e9fb87cf53f
UnderdogDG <sugarskullgb@gmail.com>
2018-10-12 22:09:56 -0500

    3D Test

--- src/js/_3dEnviroment.js
+++ src/js/_3dEnviroment.js
@@ -13,7 +13,10 @@ import { Scene,
          TextureLoader,
          MeshBasicMaterial,
          AmbientLight,
-         Fog
+         Fog,
+         Group, 
+         MeshToonMaterial,
+         SpotLight
        } from 'three';
 
       //  import * as THREE from 'three';
@@ -34,7 +37,7 @@ scene.fog = new Fog(0x001a2d, 6, 10);
 
 var camera = new PerspectiveCamera( 45, width / height, 3, 12 );
 
-var renderer = new WebGLRenderer({canvas: preview, alpha: true});
+var renderer = new WebGLRenderer({canvas: preview, alpha: true, antialias: true});
 renderer.shadowMap.enabled = true;
 renderer.setSize( width, height );
 renderer.setClearColor( 0x000000, 0 );
@@ -42,79 +45,75 @@ renderer.setClearColor( 0x000000, 0 );
 var ambient = new AmbientLight(0xFF5A3C);
 				scene.add(ambient);
 
-var light = new DirectionalLight( 0xffffff, 1, 20 );
-light.position.set( 0, 1, 0 ); 			//default; light shining from top
+var light = new SpotLight( 0xf5f5f5, 0.5, 0, 0.8, 0.5, 1.2);
+light.position.set( 10, 10, 10 ); 			//default; light shining from top
 light.castShadow = true;            // default false
 renderer.shadowMap.type = PCFSoftShadowMap;
 scene.add( light );
 
+var moonLight2 = new THREE.PointLight(0xffffff, 0.6, 150);
+moonLight2.position.set(0, 3.5, 0.7);
+moonLight2.castShadow= true;
+  scene.add(moonLight2);
+  var sphereSize = 0.5;
+var pointLightHelper = new THREE.PointLightHelper( moonLight2, sphereSize );
+scene.add( pointLightHelper );
+
 //Set up shadow properties for the light
-light.shadow.mapSize.width = 512;  // default
-light.shadow.mapSize.height = 512; // default
+light.shadow.mapSize.width = 1024;  // default
+light.shadow.mapSize.height = 1024; // default
 light.shadow.camera.near = 0.5;    // default
-light.shadow.camera.far = 500;
+light.shadow.camera.far = 100;
 
 var geometry = new BoxGeometry( 1, 1, 1 );
 var material = new MeshPhongMaterial( {color: 0x717171,side: DoubleSide} );
 var cube = new Mesh( geometry, material );
 cube.castShadow = true; //default is false
-cube.receiveShadow = false; //default
-scene.add( cube );
+cube.receiveShadow = true; //default
+cube.position.set(0, 2, 0);
+// scene.add( cube );
 
 camera.position.z = 8;
 camera.position.y = 1;
 
-let ragnar;
+let ragnar = new Group();
 const loader = new THREE.OBJLoader();
 const texture = new TextureLoader().load('./model/defaultMat_Base_Color.png');
-const skin = new MeshBasicMaterial({map: texture, side: DoubleSide});
+const normalMap = new TextureLoader().load('./model/Head_retopo_uv_F-NM_u0_v0_copy.tif');
+const Ao = new TextureLoader().load('./model/defaultMat_Mixed_AO.png');
+const skin = new MeshPhongMaterial({ map:texture, color: 0xdcdcdc, reflectivity: 0.8, shininess: 0, specular: 0xadff2f, aoMap: Ao, normalMap: normalMap});
 
 loader.load( 'model/Ragnar.obj', 
-      function(model){
-        ragnar = model;
+      model=>{
+        model.castShadow = false;
+        model.receiveShadow = false;
         model.traverse( function (node){
           if (node.isMesh) node.material = skin;
         });
-        model.scale.set(0.5,0.5,0.5);
-        model.position.set (0, -2, 0);
-        model.castShadow = true;
-        model.receiveShadow = true;
-        scene.add(model);
+        ragnar.specular = 0xadff2f;
+        ragnar.add(model);
+        
+        // model.scale.set(0.5,0.5,0.5);
+        // model.position.set (0, -2, 0);
+        // model.castShadow = true;
+        // model.receiveShadow = true;
+        // scene.add(model);
 
         // ragnar = new Mesh(model, skin);
         // ragnar.scale.set(0.5,0.5,0.5);
         // ragnar.position.set(0, -2, 0);
         // scene.add(ragnar);
       },
-      function ( xhr ) {
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-      },
+      xhr=>{ console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
       // called when loading has errors
-      function ( error ) {
-        console.log( 'An error happened' );
-      }
+      error=>{ console.log( 'An error happened' ); }
  );
 
-
- let other = loader.load( 
-   'model/Ragnar.obj',
-   model => {
-    model.scale.set(0.2,0.2,0.2);
-    model.position.set (0, -2, 0);
-    return model
-    },
-   xhr => {console.log('Other: ', ( xhr.loaded / xhr.total * 100 ) + '% loaded' )},
-   error => {console.log( 'An error happened' )}
- );
-
- console.log("OBJ: ",other);
-
- const mod = new Mesh( other, material );
-
-  mod.scale.set(0.2,0.2,0.2);
-  mod.position.set (0, -2, 0);
-
- scene.add(mod);
+        ragnar.scale.set(0.5,0.5,0.5);
+        ragnar.position.set(0, -2, 0);
+        ragnar.castShadow = true;
+        ragnar.receiveShadow = true;
+        scene.add(ragnar);
 
 
 function animate() {
